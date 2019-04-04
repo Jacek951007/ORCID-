97b18badff01eafb9a662583f721a01665c509bb
Jacek <jacek951007@gmail.com>
2019-04-04 14:11:39 +0200

    Connection between ORCID and Scopus, Downloading works correctly

--- src/main/java/com/zutorcid/Controller/GetWorks.java
+++ src/main/java/com/zutorcid/Controller/GetWorks.java
@@ -1,6 +1,6 @@
 package com.zutorcid.Controller;
 
-import com.zutorcid.Works.*;
+import com.zutorcid.Works.GetAllWorks;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -8,7 +8,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GetWorks {
+class GetWorks {
     public List<Integer> getWorks(String path, int yearStart, int yearEnd) {
 
         Token myToken = new Token();
@@ -21,16 +21,21 @@ public class GetWorks {
                 .scheme("https").host("pub.orcid.org").path("/v2.1/" + path + "/works")
                 .queryParam("Authorization", token)
                 .build(true);
-        GetAllWorks allWorks = new RestTemplate().getForObject(uriComponents.toUriString(), GetAllWorks.class);
+        GetAllWorks allWorks;
+        allWorks = new RestTemplate().getForObject(uriComponents.toUriString(), GetAllWorks.class);
 
+        int workYear=0;
 
-        for (int i = 0; i < allWorks.getGroup().size(); i++) {
-            /*if (Integer.valueOf(allWorks.getGroup().get(i).getWorkSummary().get(0).getPublicationDate().getYear().getValue())
-                    >= yearStart && Integer.valueOf(allWorks.getGroup().get(i).getWorkSummary().get(0).getPublicationDate().getYear().getValue())
-                    <= yearEnd) {*/
-                putCode.add(Integer.valueOf(allWorks.getGroup().get(i).getWorkSummary().get(0).getPutCode()));
-           // }
-        }
+         if(allWorks.getGroup() != null) {
+             for(int i=0;i<allWorks.getGroup().size();i++){
+                 if(allWorks.getGroup().get(i).getWorkSummary().get(0).getPublicationDate() != null && allWorks.getGroup().get(i).getWorkSummary().get(0).getPublicationDate().getYear() != null) {
+                     workYear = Integer.parseInt(allWorks.getGroup().get(i).getWorkSummary().get(0).getPublicationDate().getYear().getValue());
+                 }
+                if (workYear >= yearStart && workYear <= yearEnd) {
+                    putCode.add(allWorks.getGroup().get(i).getWorkSummary().get(0).getPutCode());
+                }
+             }
+         }
         return putCode;
     }
 }
